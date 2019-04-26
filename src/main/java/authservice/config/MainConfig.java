669a655c38592559a584c45e18c6e7131476ac3d
Joris Eijmberts <j.eijmberts@student.fontys.nl>
2019-04-26 15:13:02 +0200

    Removed activemq from auth-service

--- src/main/java/authservice/config/MainConfig.java
+++ src/main/java/authservice/config/MainConfig.java
@@ -1,25 +1,11 @@
 package authservice.config;
 
-import authservice.messaging.MessageSender;
-import org.apache.activemq.ActiveMQConnectionFactory;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.jms.DefaultJmsListenerContainerFactoryConfigurer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
-import org.springframework.jms.config.JmsListenerContainerFactory;
-import org.springframework.jms.connection.CachingConnectionFactory;
-import org.springframework.jms.core.JmsTemplate;
-import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
-import org.springframework.jms.support.converter.MessageConverter;
-import org.springframework.jms.support.converter.MessageType;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import javax.annotation.PostConstruct;
-import javax.annotation.PreDestroy;
-import javax.jms.ConnectionFactory;
 import java.security.SecureRandom;
 
 @Configuration
@@ -41,51 +27,5 @@ public class MainConfig {
         return new BCryptPasswordEncoder(passwordStrenght, passwordRandom);
     }
 
-    @Bean
-    public JmsListenerContainerFactory<?> myFactory(ConnectionFactory connectionFactory,
-                                                    DefaultJmsListenerContainerFactoryConfigurer configurer) {
-        DefaultJmsListenerContainerFactory factory = new DefaultJmsListenerContainerFactory();
-        // This provides all boot's default to this factory, including the message converter
-        configurer.configure(factory, connectionFactory);
-        // You could still override some of Boot's default if necessary.
-        return factory;
-    }
-
-    @Bean // Serialize message content to json using TextMessage
-    public MessageConverter jacksonJmsMessageConverter() {
-        MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
-        converter.setTargetType(MessageType.TEXT);
-        converter.setTypeIdPropertyName("_type");
-        return converter;
-    }
-
-    @Value("${spring.activemq.broker-url}")
-    private String brokerUrl;
-
-    @Bean
-    public ActiveMQConnectionFactory senderActiveMQConnectionFactory() {
-        ActiveMQConnectionFactory activeMQConnectionFactory =
-                new ActiveMQConnectionFactory();
-        activeMQConnectionFactory.setBrokerURL(brokerUrl);
-
-        return activeMQConnectionFactory;
-    }
-
-    @Bean
-    @Primary
-    public CachingConnectionFactory cachingConnectionFactory() {
-        return new CachingConnectionFactory(
-                senderActiveMQConnectionFactory());
-    }
-
-    @Bean
-    public JmsTemplate jmsTemplate() {
-        return new JmsTemplate(cachingConnectionFactory());
-    }
-
-    @Bean
-    public MessageSender sender() {
-        return new MessageSender();
-    }
 
 }
