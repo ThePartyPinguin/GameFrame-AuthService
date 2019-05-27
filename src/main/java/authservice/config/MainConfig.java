aae6c464706fd5fa5948df4d613d8323e0b1308b
Joris Eijmberts <j.eijmberts@student.fontys.nl>
2019-05-27 21:51:50 +0200

    Small fixes to solve sonar issues

--- src/main/java/authservice/config/MainConfig.java
+++ src/main/java/authservice/config/MainConfig.java
@@ -4,6 +4,7 @@ import authservice.dao.AuthChache;
 import authservice.dao.IAuthDao;
 import authservice.dao.jpa.IAuthJpaDao;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
@@ -17,14 +18,16 @@ import java.security.SecureRandom;
 @Configuration
 public class MainConfig {
 
+    @Value("${jwt.random.seed}")
+    private String secureRandomSeed;
+
 
     private static SecureRandom passwordRandom;
     private static int passwordStrenght;
 
     @PostConstruct
     public void init(){
-
-        passwordRandom = new SecureRandom("987987".getBytes());
+        passwordRandom = new SecureRandom(secureRandomSeed.getBytes());
         passwordStrenght = 5;
     }
 
